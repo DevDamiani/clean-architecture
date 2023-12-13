dd096621ad7063a770ded0747122a85c8057a2bd
Douglas Damiani <douglas.damiani@liferay.com>
2023-12-13 12:13:09 -0300

    set coverage to all files

--- domain/jest.config.cjs
+++ domain/jest.config.cjs
@@ -2,6 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
     "global": {
       "branches": 60,
